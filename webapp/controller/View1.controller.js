0f6c0f63a435fe85db34d10a5a8edc4259eb5c11
Krishna Kishor Kammaje <kkammaje@convergentinfo.com>
2018-07-24 07:08:36 +0000

    Removed Smart fields

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -4,7 +4,9 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("ZHR_VACATION_REPORT.controller.View1", {
-
+		handleExcelButtonPressed: function() {
+			sap.m.URLHelper.redirect("/sap/opu/odata/sap/ZVACATION_REPORT_SRV/QuotaRecords?$format=xlsx&$select=PersonnelNumber%2cName%2cQuotaName%2cUsed%2cRemaining%2cAnnualEntitlement%2cUnit", true);
+		}
 	});
 
 });
